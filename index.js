dbc6124c8d4ebae3aafc73110aef506b995e1e3a
sakhawat236hossain <hmdsakhawat236@gmail.com>
2025-11-09 19:51:47 +0600

    project setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors'); // ⚠️ cors import করতে হবে
+const app = express();
+const port = 8000;
+
+// Middleware
+app.use(cors());
+app.use(express.json()); 
+
+app.get('/', (req, res) => {
+  res.send('Server is running fine');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
