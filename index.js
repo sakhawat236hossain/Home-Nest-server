d41bf6fe417facb8c490233e12bc0f67e32dd247
sakhawat236hossain <hmdsakhawat236@gmail.com>
2025-11-09 21:37:12 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const cors = require('cors'); // ⚠️ cors import করতে হবে
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb'); 
 const app = express();
 const port = 8000;
 
@@ -7,9 +8,32 @@ const port = 8000;
 app.use(cors());
 app.use(express.json()); 
 
-app.get('/', (req, res) => {
-  res.send('Server is running fine');
+
+
+const uri = "mongodb+srv://home-nest:aIwwBkRBO5x7M1Is@cluster0.kwvqtmc.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('Server is running fine');
 });
