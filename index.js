d0498e67cf6a495bc266eb2dd8391b25f38468c4
sakhawat236hossain <hmdsakhawat236@gmail.com>
2025-11-10 17:16:07 +0600

    add aprpperty api done

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb'); 
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = 8000;
-require('dotenv').config();
+require("dotenv").config();
 // Middleware
 app.use(cors());
-app.use(express.json()); 
-
-
-
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kwvqtmc.mongodb.net/?appName=Cluster0`;
 
@@ -18,40 +15,56 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-
     await client.connect();
 
+    const db = client.db("homeNestDB");
+    const propertyCollection = db.collection("properties");
 
-const db =client.db("homeNestDB")
-const propertyCollection=db.collection("properties")
+    // get all data in data base
+    app.get("/allProperties", async (req, res) => {
+      const result = await propertyCollection
+        .find()
+        .sort({ price: -1 })
+        .toArray();
+      res.send(result);
+    });
 
+    // gat single data in data base
+    app.get("/singleProperty/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await propertyCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
 
-// get all data in data base
-app.get("/allProperties",async(req,res)=>{
-    const result = await propertyCollection.find().sort({price:-1}).toArray()
-    res.send(result)
-})
+    //latest property
+    app.get("/latestProperty", async (req, res) => {
+      const result = await propertyCollection
+        .find()
+        .sort({ createdAt: "asc" })
+        .limit(8)
+        .toArray();
+      res.send(result);
+    });
 
-// gat single data in data base
-app.get("/singleProperty/:id",async(req,res)=>{
-    const {id} = req.params
-    const result =await propertyCollection.findOne({_id:new ObjectId(id)})
-    res.send(result)
-})
+    // add property
+    app.post("/addProperty", async (req, res) => {
+      const property = req.body;
 
-//latest property
-app.get("/latestProperty",async(req,res)=>{
-  const result =await propertyCollection.find().sort({createdAt:"asc"}).limit(8).toArray()
-  res.send(result)
-})
+      const result = await propertyCollection.insertOne(property);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -59,10 +72,9 @@ app.get("/latestProperty",async(req,res)=>{
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Server is running fine');
+app.get("/", (req, res) => {
+  res.send("Server is running fine");
 });
-app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
