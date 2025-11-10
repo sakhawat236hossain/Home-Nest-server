892e7de0d63155531719a0fa74d7c15c6568e7f6
sakhawat236hossain <hmdsakhawat236@gmail.com>
2025-11-10 22:49:23 +0600

    my property api created

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ async function run() {
       res.send(result);
     });
 
+
+    app.get("/myProperties",async (req,res)=>{
+      const email =req.query.email
+      const result =await propertyCollection.find({userEmail: email}).toArray()
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
