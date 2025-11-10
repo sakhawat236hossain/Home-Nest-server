2626fc9427546d9259c51f225def2b5693770686
sakhawat236hossain <hmdsakhawat236@gmail.com>
2025-11-10 09:32:28 +0600

    single property api cretaed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb'); 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb'); 
 const app = express();
 const port = 8000;
 require('dotenv').config();
@@ -33,9 +33,18 @@ const propertyCollection=db.collection("properties")
 
 // get all data in data base
 app.get("/allProperties",async(req,res)=>{
-    const result = await propertyCollection.find().toArray()
+    const result = await propertyCollection.find().sort({price:-1}).toArray()
     res.send(result)
 })
+
+// gat single data in data base
+app.get("/singleProperty/:id",async(req,res)=>{
+    const {id} = req.params
+    const result =await propertyCollection.findOne({_id:new ObjectId(id)})
+    res.send(result)
+})
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -49,6 +58,7 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('Server is running fine');
 });
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
